06873a7c6f010c60bac49d881a1f0a2b321e9a37
Virginia Lombardi <virginialombardii@gmail.com>
2025-04-07 18:30:39 -0300

    Creation of Sistem, Adding attributes to sistem.

---
+++ ObligatorioP2/Dominio/Sistema.cs
@@ -0,0 +1,13 @@
+namespace Dominio;
+
+public class Sistema
+{
+    private List<Aeropuerto> _aeropuertos = new List<Aeropuerto>();
+    private List<Ruta> _rutas = new List<Ruta>();
+    private List<Avion> _aviones = new List<Avion>();
+    private List<Vuelo> _vuelos = new List<Vuelo>();
+    private List<Pasaje> _pasajes = new List<Pasaje>();
+    
+    
+    
+}
--- ObligatorioP2/ObligatorioP2/Program.cs
+++ ObligatorioP2/ObligatorioP2/Program.cs
@@ -1,9 +1,83 @@
-﻿namespace ObligatorioP2;
+﻿using Dominio;
+
+namespace ObligatorioP2;
 
 class Program
 {
+    static Sistema miSistema;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        miSistema = new Sistema();
+
+        string opcion = "";
+        while (opcion != "0")
+        {
+            MostrarMenu();
+            Console.Write("Ingrese una opcion -> ");
+            opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    PressToContinue();
+                    break;
+                case "2":
+                    PressToContinue();
+                    break;
+                case "3":
+                    PressToContinue();
+                    break;
+                case "4":
+                    PressToContinue();
+                    break;
+                case "5":
+                    PressToContinue();
+                    break;
+                case "6":
+                    PressToContinue();
+                    break;
+                case "0":
+                    Console.WriteLine("Saliendo del Sistema...");
+                    break;
+                default:
+                    MostrarError("ERROR: La opción NO es válida");
+                    PressToContinue();
+                    break;
+            }
+        }
+    }
+    
+    static void MostrarMenu()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("****** MENU ******");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("1 - Crear nueva marca");
+        Console.WriteLine("2 - Listado de marcas");
+        Console.WriteLine("3 - Listado de autos");
+        Console.WriteLine("4 - Crear nuevo auto");
+        Console.WriteLine("5 - Autos por año");
+        Console.WriteLine("0 - Salir");
+    }
+    
+    static void PressToContinue()
+    {
+        Console.WriteLine("Presione una tecla para continuar");
+        Console.ReadKey();
+    }
+    
+    static void MostrarError(string mensaje)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(mensaje);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+    static void MostrarExito(string mensaje)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(mensaje);
+        Console.ForegroundColor = ConsoleColor.Gray;
     }
 }
