dc904c27def2460f269bf4ab6cc6441fa04e9d5c
Virginia Lombardi <virginialombardii@gmail.com>
2025-06-17 01:07:45 -0300

    fixing and adding somethings about passages.

--- ObligatorioP2/Dominio/Pasaje.cs
+++ ObligatorioP2/Dominio/Pasaje.cs
@@ -2,7 +2,7 @@ using Dominio.Interfaces;
 
 namespace Dominio;
 
-public class Pasaje : IValidable, IComparable<Pasaje>
+public class Pasaje : IValidable, IComparable<Pasaje>,IComparer<Pasaje>
 {
     private int _id;
     private static int s_ultId = 1;  
@@ -64,6 +64,12 @@ public class Pasaje : IValidable, IComparable<Pasaje>
     public int CompareTo(Pasaje? other)
     {
         return this._fecha.CompareTo(other._fecha);
+    }
+
+
+    public int Compare(Pasaje? x, Pasaje? y)
+    {
+        return this._precio.CompareTo(x._precio) * -1;
     }
 
     public override string ToString()
--- ObligatorioP2/WebP2/Controllers/PasajesController.cs
+++ ObligatorioP2/WebP2/Controllers/PasajesController.cs
@@ -1,6 +1,7 @@
 
 using Dominio;
 using Microsoft.AspNetCore.Mvc;
+
 namespace WebP2.Controllers
 
 {
@@ -24,27 +25,37 @@ namespace WebP2.Controllers
                 TempData["Error"] = ex.Message;
                 return RedirectToAction("Listado", "Vuelos");
             }
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult HacerCompra(string numeroVuelo, DateTime fechaVuelo, Equipaje tipoEquipaje, double precioPasaje)
+        public IActionResult HacerCompra(string numeroVuelo, DateTime fechaVuelo, Equipaje tipoEquipaje,
+            double precioPasaje)
         {
             try
             {
-                if ((HttpContext.Session.GetString("rol")==null)|| (HttpContext.Session.GetString("rol") != "Cliente"))
+                if (HttpContext.Session.GetString("rol") == null || HttpContext.Session.GetString("rol") != "Cliente")
                 {
                     throw new Exception("Solo los clientes registrados pueden comprar pasajes.");
                 }
-                Pasaje pasajeComprado = miSistema.ComprarPasaje(numeroVuelo, fechaVuelo, HttpContext.Session.GetString("email"), tipoEquipaje, precioPasaje);
+
+                Pasaje pasajeComprado = miSistema.ComprarPasaje(numeroVuelo, fechaVuelo,
+                    HttpContext.Session.GetString("email"), tipoEquipaje, precioPasaje);
                 TempData["Exito"] = "Usted ha comprado el pasaje de manera exitosa.";
                 return RedirectToAction("Listado", "Vuelos");
             }
             catch (Exception ex)
             {
                 TempData["Error"] = ex.Message;
-                return RedirectToAction("HacerCompra", new { numeroVuelo = numeroVuelo });
+                return RedirectToAction("HacerCompra", "Pasajes");
             }
+        }
+
+        public IActionResult ListadoPasajesCliente()
+        {
+            ViewBag.Pasajes = miSistema.Pasajes;
+            return View();
         }
     }
 }
